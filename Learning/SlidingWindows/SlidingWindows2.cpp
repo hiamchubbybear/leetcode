fa6daab957647a14e6589d1513789dde2aa7cf22
Chessy <ilovepakpak@gmail.com>
2024-11-17 01:11:47 +0700

    17/11/2024

--- Learning/SlidingWindows/SlidingWindows2.cpp
+++ Learning/SlidingWindows/SlidingWindows2.cpp
@@ -1,17 +1,23 @@
 #include<iostream>
 using namespace std;
 int main() {
-    int n, k,count=0;
+    int n, k, count = 0, temp = 0;
     cin >> n >> k;
     int vect[100];
     for (int i = 0; i < n; i++)
         cin >> vect[i];
-    for(int i=0 ; i < n ; i++) {
-        if(vect[i] <= k ) count++;
+    for (int i = 0; i < n; i++) {
+        if (vect[i] <= k) count++;
     }
-    for(int i =0 ; i < n ; i ++ ) {
-            
+    for (int i = 0; i < count; i++) {
+        if (vect[i] <= k) temp++;
     }
-
-    return 9;
+    int ans = temp;
+    for (int i = count; i < n; i++) {
+        if (vect[i - count] <= k) temp--;
+        if (vect[i] <= k) temp++;
+        ans = max(temp, ans);
+    }
+    cout << count - ans;
+    return 0;
 }
